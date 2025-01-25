0c26b565fffd6be9b77800c1606cb48f86153862
PrashanRaii <prashanrai16@gmail.com>
2025-01-25 07:30:45 +0545

    initial commit

--- app.py
+++ app.py
@@ -28,5 +28,9 @@ def about():
 def login():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
